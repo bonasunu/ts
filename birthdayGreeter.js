bd7ff651ec54859d5b4373d104ea324693f0ba2f
bonasunu <bonaventurasunu@gmail.com>
2021-07-21 22:41:26 +0700

    init birthdayGreeter

---
+++ birthdayGreeter.js
@@ -0,0 +1,6 @@
+var birthdayGreeter = function (name, age) {
+    return "Happy birthday " + name + ", you are now " + age + " years old!";
+};
+var birthdayHero = "Diana";
+var age = 23;
+console.log(birthdayGreeter(birthdayHero, age));
---
+++ birthdayGreeter.ts
@@ -0,0 +1,8 @@
+const birthdayGreeter = (name: string, age: number): string => {
+	return `Happy birthday ${name}, you are now ${age} years old!`;
+}
+
+const birthdayHero = "Diana"
+const age = 23
+
+console.log(birthdayGreeter(birthdayHero, age))
